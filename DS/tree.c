6adf8e9800b2473c1d9345135bca0dc7c855404c
Arun Prakash Jana <engineerarun@gmail.com>
2015-08-20 22:20:52 +0530

    Coding style fixes using checkpatch.pl
    Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

--- DS/tree.c
+++ DS/tree.c
@@ -1,26 +1,26 @@
 #include "tree.h"
 
-tree_p* generate_tree(int* arr, int len)
+tree_p *generate_tree(int *arr, int len)
 {
 	int i = 0;
-	tree_p *head;
+	tree_p *head = NULL;
+
 	if (!arr || !len) {
 		printf("Error: Invalid array.\n");
 		return NULL;
 	}
-	
+
 	tree_p root = NULL;
+
+	root = (tree_p)malloc(sizeof(tree));
 	if (!root) {
-		root = (tree_p)malloc(sizeof(tree));
-		if (!root) {
-			printf("Error: Could not malloc().\n");
-			return NULL;
-		}
-		root->data = arr[0];
-		root->left = root->right = NULL;
-		head = (tree_p*)malloc(sizeof(tree_p));
-		*head = root;
+		printf("Error: Could not malloc().\n");
+		return NULL;
 	}
+	root->data = arr[0];
+	root->left = root->right = NULL;
+	head = malloc(sizeof(tree_p));
+	*head = root;
 
 	for (i = 1; i < len; ++i) {
 		if (!root) {
@@ -31,26 +31,26 @@ tree_p* generate_tree(int* arr, int len)
 			while (root) {
 				if (arr[i] < root->data) {
 					if (!root->left) {
-						root->left = (tree_p)malloc(sizeof(tree));
+						root->left
+							= malloc(sizeof(tree));
 						root = root->left;
 						root->data = arr[i];
 						root->left = root->right = NULL;
 						break;
-					} else {
-						root = root->left;
-						//continue;
 					}
+
+					root = root->left;
 				} else if (arr[i] >= root->data) {
 					if (!root->right) {
-						root->right = (tree_p)malloc(sizeof(tree));
+						root->right
+							= malloc(sizeof(tree));
 						root = root->right;
 						root->data = arr[i];
 						root->left = root->right = NULL;
 						break;
-					} else {
-						root = root->right;
-						//continue;
 					}
+
+					root = root->right;
 				}
 			}
 		}
@@ -61,33 +61,36 @@ tree_p* generate_tree(int* arr, int len)
 int delete_tree(tree_p root)
 {
 	int count = 0;
-	//printf("In delete_tree().\n");
+
 	if (!root) {
 		printf("Error: Invalid root pointer.\n");
 		return -1;
 	}
+
 	if (root->left)
 		count += delete_tree(root->left);
+
 	if (root->right)
 		count += delete_tree(root->right);
-	//printf("deleting a node.\n");
+
 	free(root);
 	++count;
+
 	return count;
 }
 
 int print_tree(tree_p root)
 {
 	int count = 0;
-	//printf("In print_tree().\n");
+
 	if (!root) {
 		printf("Error: Invalid root pointer.\n");
 		return -1;
 	}
 
 	printf("data: %d.\n", root->data);
-	++count; 
-	
+	++count;
+
 	if (root->left)
 		count += print_tree(root->left);
 	if (root->right)
