b8791a1ff3a9a5b691baf8ea868243bcd49eaf47
Arun Prakash Jana <engineerarun@gmail.com>
2015-10-25 20:50:19 +0530

    Include and fix dfs.c initial commit.
    Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

--- DS/dfs.c
+++ DS/dfs.c
@@ -1,11 +1,11 @@
-#include <tree.h>
-#include <stck.h>
+#include "tree.h"
+#include "stack.h"
 
 int search_DFS(tree_pp root, int val)
 {
 	int ret = FALSE;
-	tree_p node= NULL;
-	stack_p stack = NULL
+	tree_p node = NULL;
+	stack_p stack = NULL;
 
 	if (!*root || !root) {
 		printf("root node is NULL.\n");
@@ -14,32 +14,34 @@ int search_DFS(tree_pp root, int val)
 
 
 	stack = get_stack();
-	if ( !push(stack, *root) {
+	if (!push(stack, *root)) {
 		printf("push failed.\n");
 		destroy_stack(stack);
 		ret = FALSE;
 	}
 
 	while ((node = pop(stack)) != NULL) {
-		if ( node->data == val)
+		if (node->data == val)
 			ret = TRUE;
 
-		if ( node->left) {
-			if (!push(stack, node->left) {
+		if (node->left) {
+			if (!push(stack, node->left)) {
 				printf(" push()failed.\n");
 				ret = FALSE;
 				break;
+			}
 		}
 
-		if ( node->right) {
-			if (!push(stack, node->right) {
+		if (node->right) {
+			if (!push(stack, node->right)) {
 				printf(" push()failed.\n");
 				ret = FALSE;
 				break;
+			}
 		}
 	}
 		
-
 	destroy_stack(stack);
+
 	return ret;
-}		 
+}
