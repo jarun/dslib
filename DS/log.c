74de1115be950ffdc5229ef5cbe7aa46f7c555d4
Arun Prakash Jana <engineerarun@gmail.com>
2015-10-26 09:21:11 +0530

    Copyright information.
    Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

--- DS/log.c
+++ DS/log.c
@@ -1,5 +1,5 @@
 /*
- * Program to unwind the stack frames of a x86_64 process using libunwind
+ * A modular, level based log file implementation
  *
  * Author: Arun Prakash Jana <engineerarun@gmail.com>
  * Copyright (C) 2014, 2015 by Arun Prakash Jana <engineerarun@gmail.com>
