d9d7bc3caa99547b3190e78b724d7b6e29bb3d2b
Arun Prakash Jana <engineerarun@gmail.com>
2016-01-04 15:48:20 +0530

    Merge tree related functions.
    Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

--- DS/tree.c
+++ DS/tree.c
@@ -19,6 +19,8 @@
  */
 
 #include "tree.h"
+#include "queue.h"
+#include "stack.h"
 
 /*
  * Generate a tree from an array of integers
@@ -142,4 +144,199 @@ int print_tree(tree_p root)
 		count += print_tree(root->right);
 
 	return count;
+}
+
+/*
+ * Tree Breadth First Search implementation
+ * Implements iterative search using a Queue
+ * Checks for a match before adding a node to Queue
+ *
+ * PARAMS
+ * ------
+ * root: pointer to the root node pointer of a tree
+ * val : value to search
+ * stop: stop if val is found
+ */
+
+int search_BFS(tree_pp root, int val, bool stop)
+{
+	tree_p node = NULL;
+	queue_p queue = NULL;
+	int ret = FALSE;
+
+	if (!root || !*root) {
+		log(ERROR, "tree or root node is NULL!\n");
+		return FALSE;
+	}
+
+	/* Check for a match in root node */
+	node = *root;
+	if (node->data == val) {
+		log(INFO, "FOUND %d\n", val);
+
+		if (stop)
+			return TRUE;
+	}
+
+	queue = get_queue();
+
+	/* Add root node to Queue */
+	if (!enqueue(queue, *root)) {
+		log(ERROR, "enqueue failed!\n");
+		destroy_queue(queue);
+		return FALSE;
+	}
+
+	/* Loop through all nodes in the Queue */
+	while ((node = dequeue(queue)) != NULL) {
+		log(INFO, "tracking...\n");
+
+		/* Process left child of node */
+		if (node->left) {
+			if (node->left->data == val) {
+				log(INFO, "FOUND %d\n", val);
+				ret = TRUE;
+
+				if (stop)
+					break;
+			}
+
+			/* Add left child to Queue */
+			if (!enqueue(queue, node->left)) {
+				log(ERROR, "enqueue failed!\n");
+				destroy_queue(queue);
+				return FALSE;
+			}
+		}
+
+		/* Process right child of node */
+		if (node->right) {
+			if (node->right->data == val) {
+				log(INFO, "FOUND %d\n", val);
+				ret = TRUE;
+
+				if (stop)
+					break;
+			}
+
+			/* Add right child to Queue */
+			if (!enqueue(queue, node->right)) {
+				log(ERROR, "enqueue failed!\n");
+				destroy_queue(queue);
+				return FALSE;
+			}
+		}
+	}
+
+	/* Report if no match was found */
+	if (!ret)
+		log(INFO, "NOT FOUND\n");
+
+	destroy_queue(queue);
+
+	return ret;
+}
+
+/*
+ * Tree Depth First Search implementation
+ * Implements iterative search using a Stack
+ * Checks for a match before adding a node to Stack
+ *
+ * PARAMS
+ * ------
+ * root: pointer to the root node pointer of a tree
+ * val : value to search
+ * stop: stop if val is found
+ */
+
+int search_DFS(tree_pp root, int val, bool stop)
+{
+	int ret = FALSE;
+	tree_p node = NULL;
+	stack_p stack = NULL;
+
+	if (!root || !*root) {
+		log(ERROR, "tree or root node is NULL.\n");
+		return FALSE;
+	}
+
+	/* Check for a match in root node */
+	node = *root;
+	if (node->data == val) {
+		log(INFO, "FOUND %d\n", val);
+
+		if (stop)
+			return TRUE;
+	}
+
+	stack = get_stack();
+
+	/* Add root node to Stack */
+	if (!push(stack, *root)) {
+		log(ERROR, "push failed!\n");
+		destroy_stack(stack);
+		return FALSE;
+	}
+
+	/* Process all valid nodes */
+	while (node) {
+		log(INFO, "tracking...\n");
+
+		/* Match and add complete
+		   left subtree to Stack */
+		while (node->left) {
+			if (node->left->data == val) {
+				log(INFO, "FOUND %d\n", val);
+				ret = TRUE;
+
+				if (stop) {
+					node = NULL;
+					break;
+				}
+			}
+
+			/* Add node to stack */
+			if (!push(stack, node->left)) {
+				log(ERROR, "push failed!\n");
+				destroy_stack(stack);
+				return FALSE;
+			}
+
+			node = node->left;
+		}
+
+		while ((node = pop(stack)) != NULL) {
+			/* Process right child of node */
+			if (node->right) {
+				if (node->right->data == val) {
+					log(INFO, "FOUND %d\n", val);
+					ret = TRUE;
+
+					if (stop) {
+						node = NULL;
+						break;
+					}
+				}
+
+				/* Add right child to Stack */
+				if (!push(stack, node->right)) {
+					log(ERROR, "push failed!\n");
+					destroy_stack(stack);
+					return FALSE;
+				}
+
+				node = node->right;
+				/* Break inner loop if there's a right node */
+				break;
+			}
+		}
+	}
+
+	/* Report if no match was found */
+	if (!ret)
+		log(INFO, "NOT FOUND\n");
+
+	destroy_stack(stack);
+
+	return ret;
 }
